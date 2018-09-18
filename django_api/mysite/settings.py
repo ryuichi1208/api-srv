801a6f6d87e4c4ce38f05ac148c38e5c02d18183
ryuichi1208 <ryucrosskey@gmail.com>
2018-09-18 21:41:03 +0900

    first commit

--- django_api/mysite/settings.py
+++ django_api/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vi0b(#q9-d84=jrj-hy6h3%c#m#92gziac#r5w+q_y@lzf%wz('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,15 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-Jp'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
